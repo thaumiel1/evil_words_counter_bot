7a825dda4cad5e7348a6ed451d3abfefc161eddc
Will Kane <williamkane562@gmail.com>
2025-04-13 17:27:07 +0100

    main functionality working now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::fmt::format;
+// std imports
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::io::{self, BufRead};
+
+// Discord bot import
 use poise::serenity_prelude as serenity;
+use serenity::http::Http;
+use serenity::model::id::UserId;
 
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -16,6 +26,67 @@ async fn age(
     Ok(())
 }
 
+#[poise::command(slash_command, prefix_command)]
+async fn evil(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: serenity::User,
+    #[description = "Word used"] word: String,
+) -> Result<(), Error> {
+    let u = user.id.to_string();
+    if !take_all_names().contains(&u) {
+        let mut file = OpenOptions::new().append(true).open("db.txt")?;
+        writeln!(file, "{}, ", u)?;
+        append_word_to_line(&user, &word)?;
+    } else {
+        append_word_to_line(&user, &word)?;
+    }
+    let response = format!("{} saying {} logged.", user.to_string(), word);
+    ctx.say(response).await?;
+    Ok(())
+}
+
+fn append_word_to_line(user: &serenity::User, word: &String) -> Result<(), Error> {
+    let mut lines: Vec<String> = io::BufReader::new(File::open("db.txt")?)
+        .lines()
+        .filter_map(Result::ok)
+        .collect();
+    let u = user.id.to_string();
+    for line in &mut lines {
+        if line.starts_with(&u) {
+            line.push_str(&format!(" {}, ", word));
+            break;
+        }
+    }
+    let mut file = File::create("db.txt")?;
+    for line in lines {
+        writeln!(file, "{}", line)?;
+    }
+    Ok(())
+}
+
+fn take_all_names() -> Vec<String> {
+    let mut names = Vec::new();
+    if let Ok(file) = File::open("db.txt") {
+        for line in io::BufReader::new(file).lines() {
+            if let Ok(line) = line {
+                if let Some(first_word) = line.split_whitespace().next() {
+                    let cleaned = first_word.trim_end_matches(',');
+                    names.push(cleaned.to_string());
+                }
+            }
+        }
+    }
+    names
+}
+
+async fn get_username_from_user_id(http: &Http, user_id: u64) -> Option<String> {
+    let user_id = UserId::new(user_id);
+    match user_id.to_user(http).await {
+        Ok(user) => Some(user.name),
+        Err(_) => None,
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
@@ -23,7 +94,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![age(), evil()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
